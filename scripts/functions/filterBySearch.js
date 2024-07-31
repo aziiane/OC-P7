72133f1f3df5cee0098f56804288c72f5e183a34
Aziz Chebil <chebilaziz16@gmail.com>
2024-07-31 16:32:13 +0200

    Added tags search in the search bar

--- scripts/functions/filterBySearch.js
+++ scripts/functions/filterBySearch.js
@@ -5,7 +5,21 @@ export default function filterBySearch(searchValue, cardsContainer) {
     const currentRecipe = getRecipeDataById(card.id);
     if (
       card.style.display !== "none" &&
-      currentRecipe.name.toLowerCase().includes(searchValue.toLowerCase())
+      (currentRecipe.name.toLowerCase().includes(searchValue.toLowerCase()) ||
+        currentRecipe.description
+          .toLowerCase()
+          .includes(searchValue.toLowerCase()) ||
+        currentRecipe.ingredients.some((ingredient) =>
+          ingredient.ingredient
+            .toLowerCase()
+            .includes(searchValue.toLowerCase())
+        ) ||
+        currentRecipe.appliance
+          .toLowerCase()
+          .includes(searchValue.toLowerCase()) ||
+        currentRecipe.ustensils.some((ustensil) =>
+          ustensil.toLowerCase().includes(searchValue.toLowerCase())
+        ))
     ) {
       card.style.display = "block";
     } else {
